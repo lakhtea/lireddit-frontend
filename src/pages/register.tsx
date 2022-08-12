191ab78e01b9a7dad4843178ba285e4f3ad66e9b
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-12 13:09:06 -0400

    set up frontend

---
+++ src/pages/register.tsx
@@ -0,0 +1,37 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import Wrapper from "../components/Wrapper";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values, handleChange }) => (
+          <Form>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                value={values.username}
+                id="username"
+                placeholder="username"
+              />
+              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
