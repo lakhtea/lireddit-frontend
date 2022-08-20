38ae05bd3219de7461708abe0da767c54b2a4b37
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-20 18:39:48 -0400

    email

---
+++ src/pages/change-password/[token].tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+
+interface ChangePasswordProps {
+  token: string;
+}
+
+const ChangePassword: NextPage<ChangePasswordProps> = ({ token }) => {
+  return <div>Token is: {token}</div>;
+};
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
+
+export default ChangePassword;
